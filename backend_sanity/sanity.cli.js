227917bf4a9f61389a7fa779a7ebdd2165c9397d
Bobbishly <ayomiderichard255@gmail.com>
2023-09-02 14:52:29 -0700

    Uploading My Portfolio Documents

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'mkl5ehxl',
+    dataset: 'production'
+  }
+})
---
+++ frontend_react/src/components/index.js
@@ -0,0 +1,12 @@
+// import NavigationDots from './NavigationDots';
+// import SocialMedia from './SocialMedia';
+// import Navbar from './Navbar/Navbar';
+export { default as Navbar } from './Navbar/Navbar';
+export { default as NavigationDots } from './NavigationDots';
+export { default as SocialMedia } from './SocialMedia';
+
+// export {
+//   // NavigationDots,
+//   // SocialMedia,
+//   Navbar,
+// };
